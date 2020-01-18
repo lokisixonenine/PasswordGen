336283cc4683bb958f918edb40cfd59eeb43b113
Jaycee Bagtas <jaycee.bagtas@gmail.com>
2020-01-17 17:42:32 -0800

    fixed location for css and js files in index.html

---
+++ assets/java/pw.js
@@ -0,0 +1,26 @@
+//random password generator using upper/lower case characters, numbers, and symbols
+function random_password_generate(max, min) {
+    //this set password length and strength
+    let pwStrength = document.getElementById("pword").value;
+
+    //define possible values for upper case, lower case, numbers, and sybmols 
+    let values = "ABCDEFGHIJKLMNOPQRSTUVWZYZabcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*()_+";
+    let password = "";
+
+    //choose letters, symbols, numbers to be used in random password
+    for (var i = 0; i <= pwStrength; i++) {
+        password = password + values.charAt(Math.floor(Math.random() * Math.floor(values.length)));
+    }
+    //outputs random password to textbox area
+    document.getElementById("display").value = password;
+}
+
+function copyPassword() {
+
+    document.getElementById("display").select();
+
+    document.execCommand("Copy");
+
+    alert("Password copied to clipboard!");
+
+}
